c2b80e5b91bc3772b8ef532c188dbb8e80a398cc
末松慈陸 <>
2021-09-13 16:05:28 +0900

    仕様3作成

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('上手');
+const fs = require("fs");
+
+fs.readFile("data/data.csv", "utf-8", (err, data) => {
+    if (err) throw console.error(err);
+    const data_list = data.split(',');
+    data_list.forEach(data => {
+        console.log(data);
+    });
+});
+
+// console.log('上手');
+
+
+
