bb21a51a2c71cce9f5a6aabb381ce5cbb9801134
末松慈陸 <>
2021-09-13 16:12:43 +0900

    仕様5作成

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 fs.readFile("data/data.csv", "utf-8", (err, data) => {
     if (err) throw console.error(err);
     const data_list = data.split(',');
-    data_list.forEach(data => {
-        console.log(data);
+    data_list.forEach((data,index) => {
+        console.log(`${index+1}: ${data}`);
     });
 });
 
