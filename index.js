e57d9ec729683a89dd4cd3cf0904a48b29fbe6ce
末松慈陸 <>
2021-09-13 16:24:43 +0900

    課題6作成

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const fs = require("fs");
 
+// コマンドライン引数の配列
+
+const ids = [];
+
+process.argv.forEach((args, index) => {
+    // node index.jsがここの配列に含まれるので無視
+    if (index<2) return
+    // コマンドライン引数はString型なのでNumberに変更
+    ids.push(Number(args));
+});
+
 fs.readFile("data/data.csv", "utf-8", (err, data) => {
     if (err) throw console.error(err);
     const data_list = data.split(',');
     data_list.forEach((data,index) => {
-        console.log(`${index+1}: ${data}`);
+        if (ids.includes(index+1)||ids.length===0) console.log(`${index+1}: ${data}`);
     });
 });
-
-// console.log('上手');
-
-
-
