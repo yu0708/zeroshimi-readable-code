40fb012a1b57e017a61d7c190791cf4596266ea6
末松慈陸 <>
2021-09-14 13:13:29 +0900

    コマンドライン引数の配列をわかりやすく命名

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const fs = require("fs");
 
 // コマンドライン引数の配列
 
-const ids = [];
+const input_args = [];
 
 process.argv.forEach((args, index) => {
     // node index.jsがここの配列に含まれるので無視
     if (index<2) return
     // コマンドライン引数はString型なのでNumberに変更
-    ids.push(Number(args));
+    input_args.push(Number(args));
 });
 
 fs.readFile("data/data.csv", "utf-8", (err, data) => {
     if (err) throw console.error(err);
     const data_list = data.split(',');
     data_list.forEach((data,index) => {
-        if (ids.includes(index+1)||ids.length===0) console.log(`${index+1}: ${data}`);
+        if (input_args.includes(index+1)||input_args.length===0) console.log(`${index+1}: ${data}`);
     });
 });
