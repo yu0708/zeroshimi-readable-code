29fbef25bf116bb4d5a79a0aa3f894ba7184dc50
末松慈陸 <>
2021-09-14 13:40:06 +0900

    整形

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const fs = require("fs");
 // コマンドライン引数の配列
 
 const input_ids = [];
-let input_user_name = ''
-
+let input_user_name = "";
 
 process.argv.forEach((args, index) => {
-    // node index.jsがここの配列に含まれるので無視
-    if (index<2) return;
-    if (index===2) input_user_name = args
-    // コマンドライン引数はString型なのでNumberに変更
-    input_ids.push(Number(args));
+  // node index.jsがここの配列に含まれるので無視
+  if (index < 2) return;
+  if (index === 2) input_user_name = args;
+  // コマンドライン引数はString型なのでNumberに変更
+  input_ids.push(Number(args));
 });
 
 fs.readFile("data/data.csv", "utf-8", (err, data) => {
-    if (err) throw console.error(err);
-    const data_list = data.split(',');
-    console.log(`ユーザー名: ${input_user_name}`)
-    data_list.forEach((data,index) => {
-        if (input_ids.includes(index+1)||input_ids.length===0) console.log(`${index+1}: ${data}`);
-    });
+  if (err) throw console.error(err);
+  const data_list = data.split(",");
+  console.log(`ユーザー名: ${input_user_name}`);
+  data_list.forEach((data, index) => {
+    if (input_ids.includes(index + 1) || input_ids.length === 0)
+      console.log(`${index + 1}: ${data}`);
+  });
 });
